5d5a6e2f6d7d26a4febf5e6a943cdc10de8238ed
L30n <P.Leon.Flz@proton.me>
2026-05-16 21:23:57 -0600

    se modifica el css

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,19 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Register from './pages/Register'; // <-- 1. Importamos tu nueva pantalla de Registro
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard'; // 👈 1. IMPORTA EL DASHBOARD
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Ruta para el Inicio de Sesión de Amigos Salvajes */}
-        <Route path="/login" element={<Login />} />
-        
-        {/* 2. Ruta para el Formulario de Registro Dinámico */}
-        <Route path="/registro" element={<Register />} />
-        
-        {/* Ruta por defecto que te manda al login al abrir la app */}
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} /> {/* 👈 2. AGREGA LA RUTA */}
       </Routes>
     </Router>
   );
